1001d19c7f62bf8559f41e5fd0643728e962d665
y-okubo <y.okubo1225@gmail.com>
2024-08-12 19:21:40 +0900

    Initial commit

--- src/main/java/com/example/springsample/controller/UserController.java
+++ src/main/java/com/example/springsample/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.springsample.controller;
 
+
+//各ライブラリやパッケージから必要なクラスとインターフェースをインポートしている
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import com.example.springsample.service.UserService;
 
 /**
  * ユーザー情報 Controller
+ * ユーザー情報に関連するリクエストを処理するコントローラークラスです
  */
 @Controller
 public class UserController {
@@ -20,6 +23,7 @@ public class UserController {
   /**
    * ユーザー情報 Service
    */
+  //UserServiceクラスをインスタンス化してメソッドを使用できるようにする記述。userServiceという変数に格納して下のコードで使用している
   @Autowired
   UserService userService;
 
@@ -28,6 +32,7 @@ public class UserController {
    * @param model Model
    * @return ユーザー情報一覧画面のHTML
    */
+  ///user/listがリクエスト(GET)された際に動くメソッド。処理をしてHTMLファイルを返している
   @RequestMapping(value = "/user/list", method = RequestMethod.GET)
   public String displayList(Model model) {
     List<User> userlist = userService.searchAll();
--- src/main/java/com/example/springsample/controller/test.java
+++
@@ -1,5 +0,0 @@
-package com.example.springsample.controller;
-
-public class test {
-    
-}
